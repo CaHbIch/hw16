dbe34c90b80bf2c3c5dc2e62daaa0e91451f1633
CaHbI4 <FILlove@mail.ru>
2022-06-29 21:17:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+from config import DATABASE
+
+app = Flask(__name__)
+
+# Конфигурация
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/model.py
@@ -0,0 +1,32 @@
+from app import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    first_name = db.Column(db.String(30), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    email = db.Column(db.String(50), unique=True)
+    role = db.Column(db.String(100), nullable=True)
+    phone = db.Column(db.String(15), unique=True)
+
+    def __repr__(self):
+        return f"<id{self.id}>"
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    name = db.Column(db.String(30), nullable=True)
+    description = db.Column(db.String, nullable=True)
+    start_date = db.Column(db.String(12), nullable=True)
+    end_date = db.Column(db.String(12), nullable=True)
+    address = db.Column(db.String(100), nullable=True)
+    price = db.Column(db.Float)
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('offer.id'))
+
+
+class Offer(db.Model):
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
